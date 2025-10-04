76f4e7cae9f4110fc027399504923095766cb516
saivijayragav <saivijayragav@gmail.com>
2025-10-04 10:26:28 +0530

    Fixed the allow origin method

--- index.py
+++ index.py
@@ -16,8 +16,9 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_methods=["POST"],
+    allow_methods=["*"],
     allow_headers=["*"],
+    allow_credentials=["*"]
 )
 
 
